2552234f73d9053adc7099d8f96c02674ee2a512
Grace Davies <grace.e.davies@hotmail.co.uk>
2015-04-01 22:13:41 +0100

    structuremap

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace AppConsole
+﻿using StructureMap;
+
+namespace AppConsole
 {
     using System;
     using System.Linq;
     using Nodes;
     using Castle.MicroKernel.Registration;
     using Castle.Windsor;
+
+    //Examples using Castle Windsor and Structure Map
 
     class Program
     {
@@ -21,19 +25,33 @@
             //root.Go(string.Empty).ToList()
             //    .ForEach(x => Console.WriteLine(x.GetType().Name));
 
-            var container = new WindsorContainer();
+            //var container = new WindsorContainer();
+            //container.Register(Component.For<INode>().ImplementedBy<B3>().Named("B3"));
+            //container.Register(Component.For<INode>().ImplementedBy<B2>().Named("B2"));
+            //container.Register(Component.For<INode>().ImplementedBy<A3>().Named("A3"));
+            //container.Register(Component.For<INode>().ImplementedBy<C1>().Named("C1").DependsOn(container.Resolve<INode>("B3")));
+            //container.Register(Component.For<INode>().ImplementedBy<B1>().Named("B1").DependsOn(new { B2 = container.Resolve<INode>("B2") }));
+            //container.Register(Component.For<INode>().ImplementedBy<A2>().Named("A2").DependsOn(new { A3 = container.Resolve<INode>("A3"), B1 = container.Resolve<INode>("B1") }));
+            //container.Register(Component.For<INode>().ImplementedBy<A1>().Named("A1").DependsOn(new { A2 = container.Resolve<INode>("A2"), C1 = container.Resolve<INode>("C1") })); 
 
-            container.Register(Component.For<INode>().ImplementedBy<B3>().Named("B3"));
-            container.Register(Component.For<INode>().ImplementedBy<B2>().Named("B2"));
-            container.Register(Component.For<INode>().ImplementedBy<A3>().Named("A3"));
-            container.Register(Component.For<INode>().ImplementedBy<C1>().Named("C1").DependsOn(container.Resolve<INode>("B3")));
-            container.Register(Component.For<INode>().ImplementedBy<B1>().Named("B1").DependsOn( new { B2 = container.Resolve<INode>("B2") }));
-            container.Register(Component.For<INode>().ImplementedBy<A2>().Named("A2").DependsOn( new { A3 = container.Resolve<INode>("A3"), B1 = container.Resolve<INode>("B1")} )); 
-            container.Register(Component.For<INode>().ImplementedBy<A1>().Named("A1").DependsOn( new { A2 = container.Resolve<INode>("A2"), C1 = container.Resolve<INode>("C1") })); 
+            //var root = container.Resolve<INode>("A1");
 
-            var root = container.Resolve<INode>("A1");
+            //root.Go(string.Empty).ToList().ForEach(x => Console.WriteLine(x.GetType().Name));
 
-            root.Go(string.Empty).ToList().ForEach(x => Console.WriteLine(x.GetType().Name));
+            var container = new Container();
+            container.Configure(r => r.For<INode>().Use<B3>().Named("B3"));
+            container.Configure(r => r.For<INode>().Use<B2>().Named("B2"));
+            container.Configure(r => r.For<INode>().Use<A3>().Named("A3"));
+            container.Configure(r => r.For<INode>().Use<C1>().Named("C1").Ctor<INode>("B3").Is(i => i.GetInstance<INode>("B3")));
+            container.Configure(r => r.For<INode>().Use<B1>().Named("B1").Ctor<INode>("B2").Is(i => i.GetInstance<INode>("B2")));
+            container.Configure(r => r.For<INode>().Use<A2>().Named("A2").Ctor<INode>("A3").Is(i => i.GetInstance<INode>("A3")).Ctor<INode>("B1").Is(i => i.GetInstance<INode>("B1")));
+            container.Configure(r => r.For<INode>().Use<A1>().Named("A1").Ctor<INode>("A2").Is(i => i.GetInstance<INode>("A2")).Ctor<INode>("C1").Is(i => i.GetInstance<INode>("C1")));   
+
+
+            //Why does this not work?!?! ;'(
+            var root2 = container.GetInstance<INode>("A1");
+
+            root2.Go(string.Empty).ToList().ForEach(x => Console.WriteLine(x.GetType().Name));
 
             Console.ReadKey();
         }
